d0d1b88b683cfc9e9b71b17ce7ca05088366d958
Rimom <rimomaguiar@gmail.com>
2019-01-21 19:44:49 +0000

    FizBuzz - Inittial Commit

---
+++ FizzBuzz.php
@@ -0,0 +1,21 @@
+<?php
+    /**
+     * FizBuzz Test
+     *
+     * show all numbers from 0 to 100 under the rulers:
+     * 1 - When the number is divisible by 3, show "Fizz"
+     * 2 - When the number is divisible by 5, show "Buzz"
+     * 3 - When the number is divisible by 3 and 5, show "FizzBuzz"
+     *
+     * @Author Rimom costa
+     * @email rimomcosta@gmail.com
+     *
+     * Use php-cli
+     *
+     */
+
+for ($i = 0; $i <= 100; $i++){
+    $result = ($i % 3) == 0 ? 'Fizz' : '';
+    $result .= ($i % 5) == 0 ? 'Buzz' : '';
+    echo ($result ?: $i) . PHP_EOL;
+}
